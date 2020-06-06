49c697efabe3158a79ad4d5be887a5f96a0e4527
Lucas Moraes <lucasmoraes2@outlook.com.br>
2020-06-06 15:53:32 -0300

    Alteração dos arquivos de config

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -20,7 +20,7 @@ class PointsController {
         const serializedPoints = points.map(point => {
             return {
                 ...point,
-                image_url: `http://192.168.0.6:3333/uploads/${point.image}`,
+                image_url: `http://localhost:3333/uploads/${point.image}`,
             };
         });
 
