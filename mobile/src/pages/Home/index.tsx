4af5550b84ddf09c66996e3c4eb88e7431006029
Lucas Moraes <lucasmoraes2@outlook.com.br>
2020-06-05 22:49:31 -0300

    Finalização da aplicação mobile

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,34 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Feather as Icon } from '@expo/vector-icons';
-import { View, ImageBackground, Text, Image, StyleSheet } from 'react-native';
+import { View, ImageBackground, Text, Image, StyleSheet, TextInput, KeyboardAvoidingView, Platform } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
 
 const Home = () => {
-    return (
-        <ImageBackground 
-            source={require('../../assets/home-background.png')} 
-            style={styles.container}
-            imageStyle={{ width: 274, height: 368 }}
-        >
-            <View style={styles.main}>
-                <Image source={require('../../assets/logo.png')} />
-                <Text style={styles.title}>Seu marketplace de coleta de resíduos</Text>
-                <Text style={styles.description}>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</Text>
-            </View>
+    const [uf, setUf] = useState('');
+    const [city, setCity] = useState('');
+    const navigation = useNavigation();
 
-            <View style={styles.footer}>
-                <RectButton style={styles.button} onPress={() => {}}>
-                    <View style={styles.buttonIcon}>
-                        <Text>
-                            <Icon name="arrow-right" color="#FFF" size={24} />
-                        </Text>
-                    </View>
-                    <Text style={styles.buttonText}>
-                        Entrar
-                    </Text>
-                </RectButton>
-            </View>
-        </ImageBackground>
+    function handleNavigateToPoints() {
+      navigation.navigate('Points', {
+        uf,
+        city,
+      });
+    }
+
+    return (
+        <KeyboardAvoidingView 
+          style={{ flex: 1 }} 
+          behavior={Platform.OS ==='ios' ? 'padding' : undefined}
+        >
+          <ImageBackground 
+              source={require('../../assets/home-background.png')} 
+              style={styles.container}
+              imageStyle={{ width: 274, height: 368 }}
+          >
+              <View style={styles.main}>
+                  <Image source={require('../../assets/logo.png')} />
+                  <View>
+                    <Text style={styles.title}>Seu marketplace de coleta de resíduos</Text>
+                    <Text style={styles.description}>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</Text>
+                  </View>
+              </View>
+
+              <View style={styles.footer}>
+                  <TextInput
+                    style={styles.input}
+                    placeholder="Digite a UF"
+                    value={uf}
+                    maxLength={2}
+                    autoCapitalize="characters"
+                    autoCorrect={false}
+                    onChangeText={setUf}
+                  />
+
+                  <TextInput
+                    style={styles.input}
+                    placeholder="Digite a Cidade"
+                    value={city}
+                    autoCorrect={false}
+                    onChangeText={setCity}
+                  />
+
+                  <RectButton style={styles.button} onPress={handleNavigateToPoints}>
+                      <View style={styles.buttonIcon}>
+                          <Text>
+                              <Icon name="arrow-right" color="#FFF" size={24} />
+                          </Text>
+                      </View>
+                      <Text style={styles.buttonText}>
+                          Entrar
+                      </Text>
+                  </RectButton>
+              </View>
+          </ImageBackground>
+        </KeyboardAvoidingView>
     );
 };
 
@@ -36,7 +73,6 @@ const styles = StyleSheet.create({
     container: {
       flex: 1,
       padding: 32,
-      backgroundColor: '#f0f0f5'
     },
   
     main: {
